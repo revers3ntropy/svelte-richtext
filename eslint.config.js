13640a891e536ce2e7210687bb65cbc34161ae86
josephcoppin <joseph.coppin@gmail.com>
2026-02-08 16:12:14 +0000

    update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,16 @@ import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
 
+
+
+
+
+
+
+
+
+
+
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
@@ -31,6 +41,43 @@ export default ts.config(
                 parser: ts.parser,
                 svelteConfig
             }
+        }
+    },
+    {
+        rules: {
+            // https://github.com/typescript-eslint/typescript-eslint/issues/2621
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    args: 'all',
+                    vars: 'all',
+                    caughtErrors: 'none',
+                    varsIgnorePattern: '^(_|[$][$])',
+                    argsIgnorePattern: '^_'
+                }
+            ],
+            //'prettier/prettier': 'error',
+            '@typescript-eslint/consistent-type-imports': [
+                'error',
+                {
+                    prefer: 'type-imports',
+                    disallowTypeAnnotations: false
+                }
+            ],
+            // so I can do `while (true)`
+            'no-constant-condition': 'off',
+            // treats <script> tags as functions or something and complains
+            // all functions are inner-ly declared
+            'no-inner-declarations': 'off',
+            // caught by other stuff and is too sensitive (e.g. `App`)
+            'no-undef': 'off',
+            // doesn't like namespaces for some reason
+            // (see this: https://stackoverflow.com/questions/58270901)
+            '@typescript-eslint/no-namespace': 'off',
+            // just kinda useful sometimes
+            '@typescript-eslint/no-empty-function': 'off',
+            'svelte/no-navigation-without-resolve': 'off'
         }
     }
 );
